61ec6d715b3cc98908e600e66796812fd440e479
JBB092 <jos.barquero@estudiantec.cr>
2025-06-01 14:31:24 -0600

    [UPDATE] Cambios en la pantalla home
    Se realizó un cambio en la pantalla home para que no aparezcan dos headers que son generados automáticamente por el layout del proyecto

--- GymTEC-Mobile/app/_layout.tsx
+++ GymTEC-Mobile/app/_layout.tsx
@@ -2,11 +2,15 @@ import { Drawer } from 'expo-router/drawer';
 
 export default function Layout() {
   return (
-    <Drawer>
-      <Drawer.Screen name="home" options={{ title: 'Inicio' }} />
-      <Drawer.Screen name="search" options={{ title: 'Buscar Clases' }} />
-      <Drawer.Screen name="sync" options={{ title: 'Sincronización' }} />
-      <Drawer.Screen name="profile" options={{ title: 'Perfil' }} />
+    <Drawer
+      screenOptions={{
+        headerShown: false, // Oculta el header por defecto en todas las pantallas
+      }}
+    >
+      <Drawer.Screen name="home" />
+      <Drawer.Screen name="search" />
+      <Drawer.Screen name="sync" />
+      <Drawer.Screen name="profile" />
     </Drawer>
   );
 }

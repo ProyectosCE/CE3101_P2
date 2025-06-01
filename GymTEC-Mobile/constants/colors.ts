b14000ab8a5da5b111c53d7fdd0c268f05fa58a8
JBB092 <jos.barquero@estudiantec.cr>
2025-05-31 22:38:30 -0600

    [ADD][UPDATE] Creación y actualización de archivos
    Se actualizó el archivo app.json para cambiar la ruta de los assets a usar en el proyecto
    
    Además, se añadieron archivos nuevos, como el logo de la aplicación, así como otros que tienen que ver con la parte visual de la aplicación móvil

---
+++ GymTEC-Mobile/constants/colors.ts
@@ -0,0 +1,10 @@
+export const Colors = {
+  primary: '#007AFF',
+  secondary: '#333333',
+  background: '#FFFFFF',
+  backgroundAlt: '#F0F6FF',
+  text: '#212121',
+  textSecondary: '#757575',
+  success: '#00C853',
+  error: '#EF5350',
+};
